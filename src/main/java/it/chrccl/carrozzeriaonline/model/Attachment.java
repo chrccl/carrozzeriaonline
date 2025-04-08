82081cf20f7ff639a4856c10730614e11b1c34cb
ChristianCecili <chri0407@gmail.com>
2025-04-08 18:58:24 +0200

    Created Attachment class

---
+++ src/main/java/it/chrccl/carrozzeriaonline/model/Attachment.java
@@ -0,0 +1,30 @@
+package it.chrccl.carrozzeriaonline.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Attachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String contentType;
+
+    private String base64Data;
+
+    private String url;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "user_mobile_phone", referencedColumnName = "user_mobile_phone"),
+            @JoinColumn(name = "license_plate", referencedColumnName = "licensePlate"),
+            @JoinColumn(name = "date_time", referencedColumnName = "dateTime")
+    })
+    private Task task;
+
+}
