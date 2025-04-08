d584df72294e74fc63ee25bfe7594237fbabae61
ChristianCecili <chri0407@gmail.com>
2025-04-08 10:40:36 +0200

    initial commit

---
+++ src/main/java/it/chrccl/carrozzeriaonline/CarrozzeriaOnlineApplication.java
@@ -0,0 +1,13 @@
+package it.chrccl.carrozzeriaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrozzeriaOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrozzeriaOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/chrccl/carrozzeriaonline/CarrozzeriaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package it.chrccl.carrozzeriaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrozzeriaOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
