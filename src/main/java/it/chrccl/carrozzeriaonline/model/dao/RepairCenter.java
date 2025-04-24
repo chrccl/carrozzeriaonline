83057bd7fe101c8ac2fc6ceff4a94155dae0d142
ChristianCecili <chri0407@gmail.com>
2025-04-24 15:45:01 +0200

    Company name of a Repair Center is now unique

--- src/main/java/it/chrccl/carrozzeriaonline/model/dao/RepairCenter.java
+++ src/main/java/it/chrccl/carrozzeriaonline/model/dao/RepairCenter.java
@@ -1,9 +1,6 @@
 package it.chrccl.carrozzeriaonline.model.dao;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -14,6 +11,7 @@ public class RepairCenter {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String companyName;
 
     private String address;
